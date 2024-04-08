1e45f1ee28471054d4d42466de13f7be7250f029
cqgong <cgong@ebi.ac.uk>
2024-04-08 15:22:25 +0100

    :sparkles: Create disease interactor shape

--- projects/reactome-cytoscape-style/src/lib/drawer/shape/disease-interactor-shape.ts
+++ projects/reactome-cytoscape-style/src/lib/drawer/shape/disease-interactor-shape.ts
@@ -4,31 +4,66 @@ import {DrawerProvider} from "../types";
 export const diseaseInteractor: DrawerProvider = (properties, {width, height, drug, disease, interactor}) => {
 
   const hover = extract(properties.global.hoverNode);
+  const select = extract(properties.global.selectNode);
+  const fill = extract(properties.global.negative);
   const t = extract(properties.global.thickness);
-  const oR = t;
-  const iR = -t;
-  const oRx = Math.min(oR, width / 2)
-  const start = width * 0.25;
+  const t4 = t * 4
+  const t2 = t * 2;
+  const h = height / 2 + t2;
+  const midH = height / 2;
+  const decorationWidth = 20;
 
   return {
+    decorators: [
+      {
+        "background-image": `
+      <path fill="${fill}" stroke-linejoin="round" stroke-linecap="round" stroke-width="${t4}" stroke="${fill}"  d="
+      M ${t2} ${midH}
+      L ${decorationWidth + t2} ${t2}
+      H ${width - (decorationWidth + t2)}
+      L ${width - t2} ${midH}
+      L ${width - (decorationWidth + t2)} ${height - t2}
+      H ${decorationWidth + t2}
+      Z
+      " />
+      `,
+      }
+    ],
+
     hover: {
       "background-image": `
-          <path fill="${hover}" stroke-linejoin="round" stroke-linecap="round"  d="
-            M 0 ${oR}
-            a ${oRx} ${oR} 0 0 1 ${oRx} -${oR}
-            h ${width / 2 - t * 2}
-            a ${oRx} ${oR} 0 0 1 ${oRx} ${oR}
-            a ${oRx} ${iR} 0 0 0 -${oRx} -${iR}
-            h -${width / 2 - t * 2}
-            a ${oRx} ${iR} 0 0 0 -${oRx} ${iR}
-             Z"/>
-`,
-      "background-position-y": -t,
-      "background-position-x": start,
-      "bounds-expansion": t,
+      <path stroke="${hover}" stroke-linejoin="round" stroke-linecap="round" stroke-width="${t4}" d="
+      M ${t2} ${midH + t2}
+      L ${decorationWidth + t2} ${t2}
+      H ${width - (decorationWidth + t2)}
+      L ${width - t2} ${midH + t2}
+      Z
+      " />
+      `,
+      "background-position-y": -t2,
+      "background-height": h,
       "background-clip": "none",
-      "background-image-containment": "over"
+      "bounds-expansion": t2,
+      "background-image-containment": "over",
+    },
+
+    select: {
+      "background-image": `
+      <path stroke="${select}" stroke-linejoin="round" stroke-linecap="round" stroke-width="${t4}" d="
+      M ${t2} 0
+      L ${decorationWidth + t2} ${midH}
+      H ${width - (decorationWidth + t2)}
+      L ${width - t2} 0
+      Z
+      " />
+      `,
+      "background-position-y": midH,
+      "background-height": h,
+      "background-clip": "none",
+      "bounds-expansion": t2,
+      "background-image-containment": "over",
     }
+
   }
 }
 
