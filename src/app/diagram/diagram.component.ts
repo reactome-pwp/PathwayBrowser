3d40ad31447fcc7399089439af4e856a0717bf2e
cqgong <cgong@ebi.ac.uk>
2024-05-22 13:54:09 +0100

    :sparkles: Split interactor from diagram and make it a component

--- src/app/diagram/diagram.component.ts
+++ src/app/diagram/diagram.component.ts
@@ -1,14 +1,4 @@
-import {
-  AfterViewInit,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  Input,
-  OnChanges,
-  Output,
-  SimpleChanges,
-  ViewChild
-} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, OnChanges, Output, SimpleChanges, ViewChild} from '@angular/core';
 import {DiagramService} from "../services/diagram.service";
 import cytoscape from "cytoscape";
 // @ts-ignore
@@ -17,17 +7,11 @@ import {DarkService} from "../services/dark.service";
 import {InteractorService} from "../interactors/services/interactor.service";
 import {delay, distinctUntilChanged, filter, Observable, share, Subject} from "rxjs";
 import {ReactomeEventTypes} from "../../../projects/reactome-cytoscape-style/src/lib/model/reactome-event.model";
-import {PsicquicResource, Resource} from "../interactors/model/interactor-entity.model";
-import {MatSelect} from "@angular/material/select";
-import {FormControl} from "@angular/forms";
 import {DiagramStateService} from "../services/diagram-state.service";
 import {UntilDestroy} from "@ngneat/until-destroy";
-import {MatDialog} from "@angular/material/dialog";
-import {
-  CustomInteractorDialogComponent
-} from "../interactors/custom-interactor-dialog/custom-interactor-dialog.component";
 import {ResourceType} from "../interactors/common/overlay-resource";
 import {extract} from "../../../projects/reactome-cytoscape-style/src/lib/properties-utils";
+import {InteractorsComponent} from "../interactors/interactors.component";
 
 @UntilDestroy({checkProperties: true})
 @Component({
@@ -40,18 +24,13 @@ export class DiagramComponent implements AfterViewInit, OnChanges {
   @ViewChild('cytoscape') cytoscapeContainer?: ElementRef<HTMLDivElement>;
   @ViewChild('cytoscapeCompare') compareContainer?: ElementRef<HTMLDivElement>;
   @ViewChild('legend') legendContainer?: ElementRef<HTMLDivElement>;
-  @ViewChild('psicquicSelect') psicquicSelect?: MatSelect;
+  @ViewChild('interactor') InteractorsComponent?: InteractorsComponent;
 
   comparing: boolean = false;
   fit = true;
-  psicquicResources: PsicquicResource[] = []
-  selectedPsicquicResource = new FormControl();
-  isDataFromPsicquicLoading: boolean = false;
-  resourceTokens: Resource[] = [];
   readonly ResourceType = ResourceType;
 
-
-  constructor(private diagram: DiagramService, public dark: DarkService, private interactorsService: InteractorService, private state: DiagramStateService, public dialog: MatDialog, private cdr: ChangeDetectorRef) {
+  constructor(private diagram: DiagramService, public dark: DarkService, private interactorsService: InteractorService, private state: DiagramStateService) {
   }
 
   cy!: cytoscape.Core;
@@ -107,7 +86,6 @@ export class DiagramComponent implements AfterViewInit, OnChanges {
 
     this.loadDiagram();
 
-    this.getPsicquicResources();
   }
 
   loadDiagram() {
@@ -134,7 +112,7 @@ export class DiagramComponent implements AfterViewInit, OnChanges {
       })
   }
 
-  private initialiseReplaceElements() {
+  public initialiseReplaceElements() {
     if (this.comparing)
       this.cy.batch(() => {
         this.cy.elements('[!isBackground]').style('visibility', 'hidden')
@@ -381,77 +359,8 @@ export class DiagramComponent implements AfterViewInit, OnChanges {
   };
 
 
-  getInteractors(resource: string) {
-    const isCustom = this.interactorsService.isCustomResource(resource, this.psicquicResources)
-    const isPsicquic = this.psicquicResources.filter(pr => pr.name != ResourceType.STATIC).some(r => r.name === this.state.get('overlay'))
-    this.cys.forEach(cy => {
-      if (!resource) return;
-
-      if (!isPsicquic) {
-        this.selectedPsicquicResource.reset();
-      }
-
-      if (!isCustom) {
-        this.interactorsService.getInteractorData(cy, resource).subscribe(interactors => {
-          this.interactorsService.addInteractorOccurrenceNode(interactors, cy, resource);
-          this.initialiseReplaceElements(); // Avoid floating occurrence nodes when in compare mode
-        });
-      }
-      this.state.set('overlay', resource)
-    })
-  }
-
-  getPsicquicResources() {
-    this.interactorsService.getPsicquicResources().subscribe(resources => {
-      this.psicquicResources = resources;
-    });
-  }
-
-  onPsicquicResourceChange(selectedResource: string) {
-    this.isDataFromPsicquicLoading = true;
-    this.interactorsService.getInteractorData(this.cy, selectedResource).subscribe(interactors => {
-      this.interactorsService.addInteractorOccurrenceNode(interactors, this.cy, selectedResource)
-      this.isDataFromPsicquicLoading = false;
-      this.psicquicSelect?.close();
-      this.state.set('overlay', selectedResource)
-    })
-  }
-
-  openCustomInteractorDialog() {
-    const dialogRef = this.dialog.open(CustomInteractorDialogComponent, {
-      data: {cy: this.cy},
-      restoreFocus: false // Deselect button when closing
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      const resource = dialogRef.componentInstance.resource
-      if (resource.token) {
-        this.resourceTokens!.push(resource)
-        this.state.set('overlay', resource.token.summary.token)
-      }
-      this.cdr.detectChanges();
-    })
-  }
-
-  deleteResource(resource: Resource) {
-    const index = this.resourceTokens!.indexOf(resource);
-    if (index !== -1) {
-      this.resourceTokens!.splice(index, 1);
-      this.cy.elements(`[resource = '${resource.token?.summary.token}']`).remove();
-    }
-  }
-
-  onCustomResourceChange(resource: Resource) {
-    this.interactorsService.sendPostRequest(resource.token!, this.cy).subscribe((result) => {
-      this.cys.forEach(cy => {
-        this.interactorsService.addInteractorOccurrenceNode(result.interactors, cy, result.interactors.resource);
-        this.state.set('overlay', resource.token!.summary.token);
-      })
-    })
-  }
-
-  isSelected(resource: Resource): boolean {
-    return this.resourceTokens!.includes(resource);
+  getInteractors(resource: string | null) {
+    this.InteractorsComponent!.getInteractors(resource);
   }
 
   updateStyle() {
@@ -506,7 +415,10 @@ export class DiagramComponent implements AfterViewInit, OnChanges {
       this.flag(this.state.get('flag'), cy);
       this.select(this.state.get("select"), cy);
     }
-    this.getInteractors(this.state.get("overlay"))
+
+    if (this.state.get("overlay")) {
+      this.InteractorsComponent!.getInteractors(this.state.get("overlay"))
+    }
   }
 
   compareBackgroundSync = this.reactomeEvents$.pipe(
---
+++ src/app/interactors/interactors.component.ts
@@ -0,0 +1,137 @@
+import {AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, Output} from '@angular/core';
+import {ResourceType} from "./common/overlay-resource";
+import cytoscape from "cytoscape";
+import {DiagramService} from "../services/diagram.service";
+import {DarkService} from "../services/dark.service";
+import {InteractorService} from "./services/interactor.service";
+import {DiagramStateService} from "../services/diagram-state.service";
+import {MatDialog} from "@angular/material/dialog";
+import {PsicquicResource, Resource} from "./model/interactor-entity.model";
+import {CustomInteractorDialogComponent} from "./custom-interactor-dialog/custom-interactor-dialog.component";
+
+@Component({
+  selector: 'cr-interactors',
+  templateUrl: './interactors.component.html',
+  styleUrls: ['./interactors.component.scss']
+})
+export class InteractorsComponent implements AfterViewInit {
+
+  isDataFromPsicquicLoading: boolean = false;
+  resourceTokens: Resource[] = [];
+  panelOpenState = false;
+  psicquicResources: PsicquicResource[] = []
+  selectedPsicquicResource: string | null = null
+  activeButton: string | null = null;
+  protected readonly ResourceType = ResourceType;
+
+  @Input('cy') cy!: cytoscape.Core;
+  @Input('cys') cys: cytoscape.Core[] = [];
+  @Output('initialiseReplaceElements') initialiseReplaceElements: EventEmitter<any> = new EventEmitter();
+
+  constructor(private diagram: DiagramService, public dark: DarkService, private interactorsService: InteractorService, private state: DiagramStateService, public dialog: MatDialog, private cdr: ChangeDetectorRef) {
+  }
+
+  setActiveButton(resource: string) {
+    this.activeButton = resource;
+  }
+
+
+  ngAfterViewInit(): void {
+    this.getPsicquicResources()
+  }
+
+  getInteractors(resource: string | null) {
+    console.log("re " , resource)
+    if (!resource) return;
+    this.setActiveButton(resource);
+    const isCustom = this.interactorsService.isCustomResource(resource, this.psicquicResources)
+    const isPsicquic = this.psicquicResources.filter(pr => pr.name != ResourceType.STATIC).some(r => r.name === this.state.get('overlay'))
+    this.cys.forEach(cy => {
+      if (!isCustom && !isPsicquic) {
+        this.interactorsService.getInteractorData(cy, resource).subscribe(interactors => {
+          this.interactorsService.addInteractorOccurrenceNode(interactors, cy, resource);
+          this.initialiseReplaceElements.emit();
+        });
+      }
+      this.state.set('overlay', resource)
+    })
+  }
+
+  getPsicquicResources() {
+    this.interactorsService.getPsicquicResources().subscribe(resources => {
+      this.psicquicResources = resources.filter(r => r.name !== ResourceType.STATIC);
+    });
+  }
+
+  onPsicquicResourceChange(selectedResource: string) {
+    this.isDataFromPsicquicLoading = true;
+    this.selectedPsicquicResource = selectedResource;
+    this.setActiveButton(selectedResource);
+    this.cys.forEach(cy => {
+      this.interactorsService.getInteractorData(cy, selectedResource).subscribe(interactors => {
+        this.interactorsService.addInteractorOccurrenceNode(interactors, cy, selectedResource);
+        this.isDataFromPsicquicLoading = false;
+        this.state.set('overlay', selectedResource);
+      });
+    });
+  }
+
+  openCustomInteractorDialog() {
+    this.cys.forEach(cy => {
+      // Avoid multiple opening dialogs
+      if (this.dialog.openDialogs.length === 1) {
+        return
+      }
+      const dialogRef = this.dialog.open(CustomInteractorDialogComponent, {
+        data: {cy: cy},
+        restoreFocus: false // Deselect button when closing
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        const resource = dialogRef.componentInstance.resource
+        if (resource.token) {
+          this.resourceTokens!.push(resource)
+          this.setActiveButton(resource.token!.summary.token)
+          this.state.set('overlay', resource.token.summary.token)
+        }
+        this.cdr.detectChanges();
+      })
+    })
+  }
+
+  isSelected(resource: Resource): boolean {
+    return this.resourceTokens!.includes(resource);
+  }
+
+  onCustomResourceChange(resource: Resource) {
+    this.interactorsService.sendPostRequest(resource.token!, this.cy).subscribe((result) => {
+      this.cys.forEach(cy => {
+        this.interactorsService.addInteractorOccurrenceNode(result.interactors, cy, result.interactors.resource);
+        this.setActiveButton(resource.token!.summary.token)
+        this.state.set('overlay', resource.token!.summary.token);
+      })
+    })
+  }
+
+  deleteResource(resource: Resource) {
+    const index = this.resourceTokens!.indexOf(resource);
+    if (index !== -1) {
+      this.resourceTokens!.splice(index, 1);
+      this.cys.forEach(cy => {
+        cy.elements(`[resource = '${resource.token?.summary.token}']`).remove();
+        this.state.set('overlay', null)
+      })
+
+    }
+  }
+
+  clearInteractors() {
+    this.cys.forEach(cy => {
+      this.interactorsService.clearAllInteractorNodes(cy);
+      this.setActiveButton('clear');
+      this.state.set('overlay', null);
+      this.selectedPsicquicResource = null;
+    })
+  }
+
+}
