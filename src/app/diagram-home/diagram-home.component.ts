69c155f1619a89c1b31ab332fc36ad603ef205bd
Eliot Ragueneau <eragueneau@ebi.ac.uk>
2024-09-12 16:09:09 +0100

    :bug: Load overlay by url

--- src/app/diagram-home/diagram-home.component.ts
+++ src/app/diagram-home/diagram-home.component.ts
@@ -1,7 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {AnalysisService} from "../services/analysis.service";
 import {DarkService} from "../services/dark.service";
-import {InteractorService} from "../interactors/services/interactor.service";
+
 
 @Component({
   selector: 'cr-diagram-home',
@@ -12,8 +12,7 @@ export class DiagramHomeComponent {
 
   @Input('id') diagramId: string = '';
 
-
-  constructor(public analysis: AnalysisService, public dark: DarkService, public interactor: InteractorService) {
+  constructor(public analysis: AnalysisService, public dark: DarkService) {
   }
 
   clearAnalysis() {
